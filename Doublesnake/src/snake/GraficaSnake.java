1f9d8161bac7dea4349c41e59200120822f1e88f
lellihna@gmail.com <lellihna@gmail.com@873a040d-b0c7-1e49-e851-3223376ac986>
2013-01-08 16:05:08 +0000

    aggiunto lo sfondo a grafica snake sistemato la difficolta la mela ora nn compare piu sotto il serpente sistemato il thread

--- Doublesnake/src/snake/GraficaSnake.java
+++ Doublesnake/src/snake/GraficaSnake.java
@@ -1,7 +1,12 @@
 package snake;
 
 import doublesnake.Names;
+import java.awt.BorderLayout;
+import java.awt.Component;
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 
 public class GraficaSnake extends JFrame {
 
@@ -9,19 +14,27 @@ public class GraficaSnake extends JFrame {
      *
      */
     private static final long serialVersionUID = 1L;
+    private JLabel labelSfondo;
 
     public GraficaSnake() {
-        Snake snake=new Snake();
-        add(snake);
-        snake.run();
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setSize(Names.WINDOWS_WHITH, Names.WINDOWS_HEIGH);
-        
-        setLocationRelativeTo(null);
-        setTitle("Snake");
 
+        setName(Names.NOME_FRAME_CREAMAPPA);
+        setTitle(Names.NOME_FRAME_CREAMAPPA);
+        setSize(Names.WINDOWS_WHITH, Names.WINDOWS_HEIGH);
+        labelSfondo = new JLabel();
+        labelSfondo.setIcon(new ImageIcon(Names.PATH_SFONDO));
+        labelSfondo.setLayout(new BorderLayout());
+        labelSfondo.add(createCenterPanel(), BorderLayout.CENTER);
+        add(labelSfondo);
+        setLocationRelativeTo(null);
         setResizable(false);
-        setVisible(true);
     }
-    
+
+    private JPanel createCenterPanel() {
+        Snake snake = new Snake();
+        add(snake);
+        snake.start();
+        snake.setOpaque(false);
+        return snake;
+    }
 }
--- Doublesnake/src/snake/Snake.java
+++ Doublesnake/src/snake/Snake.java
@@ -13,26 +13,25 @@ import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 import java.util.Hashtable;
 import javax.swing.ImageIcon;
-import javax.swing.JLabel;
+import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.Timer;
+import menu.Opzioni;
 
-public class Snake extends JPanel implements ActionListener, Runnable{
+public class Snake extends JPanel implements ActionListener, Runnable {
 
     /**
      *
      */
     private static final long serialVersionUID = 1L;
-    private final int ALL_DOTS = 900;
-    private final int RAND_POSX = 20;
-    private final int RAND_POSY = 20;
-    private final int DELAY = 200;
+    private final int ALL_DOTS = 672;
+    private int DELAY;
     private int x[] = new int[ALL_DOTS];
     private int y[] = new int[ALL_DOTS];
     private int dots;
     private int apple_x;
     private int apple_y;
-    private boolean left =false;
+    private boolean left = false;
     private boolean right = true;
     private boolean up = false;
     private boolean down = false;
@@ -40,87 +39,105 @@ public class Snake extends JPanel implements ActionListener, Runnable{
     private Timer timer;
     private Image apple;
     private Hashtable<String, Image> snake;
-    
 
     public Snake() {
-        
+
         addKeyListener(new TAdapter());
-        snake=new Hashtable<String, Image>();
-        
-        JLabel sfondo=new JLabel();
-        sfondo.setBounds(0, 0, Names.PANNELLO_WIDTH, Names.PANNELLO_HEIGHT);
-        sfondo.setIcon(new ImageIcon(Names.PATH_CAMPO_COMETA));
-        sfondo.setVisible(true);
-        add(sfondo);
-        
-        setBounds(0, 0, Names.PANNELLO_WIDTH, Names.PANNELLO_HEIGHT);
-        
+        snake = new Hashtable<String, Image>();
+
+        //JLabel sfondo=new JLabel();
+        //sfondo.setBounds(0, 0, Names.PANNELLO_WIDTH, Names.PANNELLO_HEIGHT);
+        //sfondo.setIcon(new ImageIcon(Names.PATH_CAMPO_COMETA));
+        //sfondo.setVisible(true);
+        //add(sfondo);
+        //setBounds(0, 0, Names.PANNELLO_WIDTH, Names.PANNELLO_HEIGHT);
+
         imageLoad(snake);
         //initGame();
     }
-    
+
     private void imageLoad(Hashtable<String, Image> snake) {
         //PALLINO
-        ImageIcon iia = new ImageIcon(this.getClass().getResource(Names.PATH_MELA));
+        ImageIcon iia = new ImageIcon(Names.PATH_MELA);
         apple = iia.getImage();
-        
+
         //testa su
-        ImageIcon idtsu = new ImageIcon(this.getClass().getResource(Names.PATH_TESTA_SU));
+        ImageIcon idtsu = new ImageIcon(Names.PATH_TESTA_SU);
         snake.put("tsu", idtsu.getImage());
 
         //testa giu
-        ImageIcon idtg = new ImageIcon(this.getClass().getResource(Names.PATH_TESTA_GIU));
+        ImageIcon idtg = new ImageIcon(Names.PATH_TESTA_GIU);
         snake.put("tg", idtg.getImage());
 
         //testa destra
-        ImageIcon idtd = new ImageIcon(this.getClass().getResource(Names.PATH_TESTA_DESTRA));
+        ImageIcon idtd = new ImageIcon(Names.PATH_TESTA_DESTRA);
         snake.put("td", idtd.getImage());
 
         //testa sinistra
-        ImageIcon idts = new ImageIcon(this.getClass().getResource(Names.PATH_TESTA_SINISTRA));
+        ImageIcon idts = new ImageIcon(Names.PATH_TESTA_SINISTRA);
         snake.put("ts", idts.getImage());
 
         //coda su 
-        ImageIcon idcsu = new ImageIcon(this.getClass().getResource(Names.PATH_CODA_SU));
+        ImageIcon idcsu = new ImageIcon(Names.PATH_CODA_SU);
         snake.put("csu", idcsu.getImage());
 
         //coda giu
-        ImageIcon idcg = new ImageIcon(this.getClass().getResource(Names.PATH_CODA_GIU));
+        ImageIcon idcg = new ImageIcon(Names.PATH_CODA_GIU);
         snake.put("cg", idcg.getImage());
 
         //coda sinistra
-        ImageIcon idcs = new ImageIcon(this.getClass().getResource(Names.PATH_CODA_SINISTRA));
+        ImageIcon idcs = new ImageIcon(Names.PATH_CODA_SINISTRA);
         snake.put("cs", idcs.getImage());
 
         //coda destra
-        ImageIcon idcd = new ImageIcon(this.getClass().getResource(Names.PATH_CODA_DESTRA));
+        ImageIcon idcd = new ImageIcon(Names.PATH_CODA_DESTRA);
         snake.put("cd", idcd.getImage());
 
         //verso alto a sinistra
-        ImageIcon idaas = new ImageIcon(this.getClass().getResource(Names.PATH_ALTO_ALTO_SINISTRA));
+        ImageIcon idaas = new ImageIcon(Names.PATH_ALTO_ALTO_SINISTRA);
         snake.put("aas", idaas.getImage());
 
         //verso alto a destra
-        ImageIcon idaad = new ImageIcon(this.getClass().getResource(Names.PATH_ALTO_ALTO_DESTRA));
+        ImageIcon idaad = new ImageIcon(Names.PATH_ALTO_ALTO_DESTRA);
         snake.put("aad", idaad.getImage());
 
         //verso basso a sinistra
-        ImageIcon idabs = new ImageIcon(this.getClass().getResource(Names.PATH_ALTO_BASSO_SINISTRA));
+        ImageIcon idabs = new ImageIcon(Names.PATH_ALTO_BASSO_SINISTRA);
         snake.put("abs", idabs.getImage());
 
         //verso basso a destra
-        ImageIcon idabd = new ImageIcon(this.getClass().getResource(Names.PATH_ALTO_BASSO_DESTRA));
+        ImageIcon idabd = new ImageIcon(Names.PATH_ALTO_BASSO_DESTRA);
         snake.put("abd", idabd.getImage());
 
         //corpo orizzontale
-        ImageIcon idmo = new ImageIcon(this.getClass().getResource(Names.PATH_MOVIMETNO_ORIZZONTALE));
+        ImageIcon idmo = new ImageIcon(Names.PATH_MOVIMETNO_ORIZZONTALE);
         snake.put("mo", idmo.getImage());
 
         //corpo verticale
-        ImageIcon idmv = new ImageIcon(this.getClass().getResource(Names.PATH_MOVIMETNO_VERTICALE));
+        ImageIcon idmv = new ImageIcon(Names.PATH_MOVIMETNO_VERTICALE);
         snake.put("mv", idmv.getImage());
-
+        setDelay();
         setFocusable(true);
+    }
+
+    public void setDelay() {
+        Opzioni opz = (Opzioni) Opzioni.getIstance(new JFrame());
+        int temp = opz.getLivello();
+        if (temp == 1) {
+            DELAY = 300;
+        }
+        if (temp == 2) {
+            DELAY = 250;
+        }
+        if (temp == 3) {
+            DELAY = 200;
+        }
+        if (temp == 4) {
+            DELAY = 125;
+        }
+        if (temp == 5) {
+            DELAY = 50;
+        }
     }
 
     public void initGame() {
@@ -141,72 +158,70 @@ public class Snake extends JPanel implements ActionListener, Runnable{
             g.drawImage(apple, apple_x, apple_y, this);
             int z;
             boolean flag;
-            for (z = 0; z < dots-1; z++){
-                flag=false;
+            for (z = 0; z < dots - 1; z++) {
+                flag = false;
                 if (z == 0) {
                     //TESTA
-                    if(left){
+                    if (left) {
                         g.drawImage(snake.get("ts"), x[z], y[z], this);
                     }
-                    if(up){
+                    if (up) {
                         g.drawImage(snake.get("tsu"), x[z], y[z], this);
                     }
-                    if(right){
+                    if (right) {
                         g.drawImage(snake.get("td"), x[z], y[z], this);
                     }
-                    if(down){
+                    if (down) {
                         g.drawImage(snake.get("tg"), x[z], y[z], this);
                     }
-                } else{
+                } else {
                     //ANGOLI
-                    if( ( (y[z+1]==y[z]) && (y[z]<y[z-1]) && (x[z+1]<x[z]) && (x[z]==x[z-1]) ) || ( (y[z+1]>y[z]) && (y[z]==y[z-1]) && (x[z+1]==x[z]) && (x[z]>x[z-1]) )){
+                    if (((y[z + 1] == y[z]) && (y[z] < y[z - 1]) && (x[z + 1] < x[z]) && (x[z] == x[z - 1])) || ((y[z + 1] > y[z]) && (y[z] == y[z - 1]) && (x[z + 1] == x[z]) && (x[z] > x[z - 1]))) {
                         g.drawImage(snake.get("aad"), x[z], y[z], this);
-                        flag=true;
+                        flag = true;
                     }
-                    if(( (y[z+1]==y[z]) && (y[z]<y[z-1]) && (x[z+1]>x[z]) && (x[z]==x[z-1]) ) || ( (y[z+1]>y[z]) && (y[z]==y[z-1]) && (x[z+1]==x[z]) && (x[z]<x[z-1]) ) ){
+                    if (((y[z + 1] == y[z]) && (y[z] < y[z - 1]) && (x[z + 1] > x[z]) && (x[z] == x[z - 1])) || ((y[z + 1] > y[z]) && (y[z] == y[z - 1]) && (x[z + 1] == x[z]) && (x[z] < x[z - 1]))) {
                         g.drawImage(snake.get("aas"), x[z], y[z], this);
-                        flag=true;
+                        flag = true;
                     }
-                    if(( (y[z+1]<y[z]) && (y[z]==y[z-1]) && (x[z+1]==x[z]) && (x[z]>x[z-1]) ) || ( (y[z+1]==y[z]) && (y[z]>y[z-1]) && (x[z+1]<x[z]) && (x[z]==x[z-1]) ) ){
+                    if (((y[z + 1] < y[z]) && (y[z] == y[z - 1]) && (x[z + 1] == x[z]) && (x[z] > x[z - 1])) || ((y[z + 1] == y[z]) && (y[z] > y[z - 1]) && (x[z + 1] < x[z]) && (x[z] == x[z - 1]))) {
                         g.drawImage(snake.get("abd"), x[z], y[z], this);
-                        flag=true;
+                        flag = true;
                     }
-                    if(( (y[z+1]==y[z]) && (y[z]>y[z-1]) && (x[z+1]>x[z]) && (x[z]==x[z-1]) ) || ( (y[z+1]<y[z]) && (y[z]==y[z-1]) && (x[z+1]==x[z]) && (x[z]<x[z-1]) ) ){
-                        g.drawImage(snake.get("abs"), x[z], y[z], this); 
-                        flag=true;
+                    if (((y[z + 1] == y[z]) && (y[z] > y[z - 1]) && (x[z + 1] > x[z]) && (x[z] == x[z - 1])) || ((y[z + 1] < y[z]) && (y[z] == y[z - 1]) && (x[z + 1] == x[z]) && (x[z] < x[z - 1]))) {
+                        g.drawImage(snake.get("abs"), x[z], y[z], this);
+                        flag = true;
                     }
-                     //CORPO
-                    if(flag==false){
-                        if(y[z]==y[z-1] && y[z]==y[z+1]) {                
+                    //CORPO
+                    if (flag == false) {
+                        if (y[z] == y[z - 1] && y[z] == y[z + 1]) {
                             g.drawImage(snake.get("mo"), x[z], y[z], this);
-                        }
-                        else {
+                        } else {
                             g.drawImage(snake.get("mv"), x[z], y[z], this);
-                        } 
+                        }
 
-                        if(x[z]==x[z-1] && x[z]==x[z+1]) {
+                        if (x[z] == x[z - 1] && x[z] == x[z + 1]) {
                             g.drawImage(snake.get("mv"), x[z], y[z], this);
-                        }
-                        else {
+                        } else {
                             g.drawImage(snake.get("mo"), x[z], y[z], this);
                         }
                     }
                 }
-            } 
+            }
             //CODA
-            if(x[z]>x[z-1]){
+            if (x[z] > x[z - 1]) {
                 g.drawImage(snake.get("cs"), x[z], y[z], this);
             }
-            if(y[z]>y[z-1]){
+            if (y[z] > y[z - 1]) {
                 g.drawImage(snake.get("csu"), x[z], y[z], this);
             }
-            if(x[z]<x[z-1]){
+            if (x[z] < x[z - 1]) {
                 g.drawImage(snake.get("cd"), x[z], y[z], this);
             }
-            if(y[z]<y[z-1]){
+            if (y[z] < y[z - 1]) {
                 g.drawImage(snake.get("cg"), x[z], y[z], this);
             }
-            
+
             Toolkit.getDefaultToolkit().sync();
             g.dispose();
 
@@ -217,11 +232,11 @@ public class Snake extends JPanel implements ActionListener, Runnable{
 
     public void gameOver(Graphics g) {
         String msg = "Game Over";
-        
+
         Font small = new Font("Helvetica", Font.BOLD, 14);
-        
+
         FontMetrics metr = this.getFontMetrics(small);
-        
+
         g.setColor(Color.white);
         g.setFont(small);
         g.drawString(msg, (Names.PANNELLO_WIDTH - metr.stringWidth(msg)) / 2, Names.PANNELLO_HEIGHT / 2);
@@ -239,14 +254,14 @@ public class Snake extends JPanel implements ActionListener, Runnable{
 
         for (int z = dots; z > 0; z--) {
             /*if(x[z]==WIDTH)
-                x[z]=0;
-            if(x[z]==0)
-                x[z]=WIDTH;
-            if(y[z]==HEIGHT)
-                y[z]=0;
-            if(y[z]==0)
-                y[z]=HEIGHT;*/
-            
+             x[z]=0;
+             if(x[z]==0)
+             x[z]=WIDTH;
+             if(y[z]==HEIGHT)
+             y[z]=0;
+             if(y[z]==0)
+             y[z]=HEIGHT;*/
+
             x[z] = x[(z - 1)];
             y[z] = y[(z - 1)];
         }
@@ -281,7 +296,7 @@ public class Snake extends JPanel implements ActionListener, Runnable{
             inGame = false;
         }
 
-        if (y[0] < 0) {
+        if (y[0] < 1) {
             inGame = false;
         }
 
@@ -289,16 +304,28 @@ public class Snake extends JPanel implements ActionListener, Runnable{
             inGame = false;
         }
 
-        if (x[0] < 0) {
+        if (x[0] < 1) {
             inGame = false;
         }
     }
 
     public void locateApple() {
-        int r = (int) (Math.random() * RAND_POSX);
-        apple_x = ((r * Names.DOT_SIZE));
-        r = (int) (Math.random() * RAND_POSY);
-        apple_y = ((r * Names.DOT_SIZE));
+        do{
+            int r = (int) (Math.random() * Names.NUMERO_RIGHE);
+            apple_x = ((r * Names.DOT_SIZE));
+            r = (int) (Math.random() * Names.NUMERO_COLONNE);
+            apple_y = ((r * Names.DOT_SIZE));
+        }while(controlApple());
+    }
+    
+    public boolean controlApple(){
+        boolean flag=false;
+        for (int i = 0; i < x.length; i++) {
+            if(x[i]==apple_x && y[i]==apple_y)
+                flag=true;
+            return flag;
+        }
+        return flag;
     }
 
     @Override
@@ -316,6 +343,12 @@ public class Snake extends JPanel implements ActionListener, Runnable{
     public void run() {
         initGame();
     }
+    
+    public void start() {
+        //Creating thread
+        Thread th = new Thread(this);
+        th.start();
+    }
 
     private class TAdapter extends KeyAdapter {
 
@@ -326,30 +359,30 @@ public class Snake extends JPanel implements ActionListener, Runnable{
 
             if ((key == KeyEvent.VK_LEFT) && (!right)) {
                 left = true;
-                up=false;
-                down=false;
-                right=false;
+                up = false;
+                down = false;
+                right = false;
             }
 
             if ((key == KeyEvent.VK_RIGHT) && (!left)) {
                 right = true;
-                up=false;
-                down=false;
-                left=false;
+                up = false;
+                down = false;
+                left = false;
             }
 
             if ((key == KeyEvent.VK_UP) && (!down)) {
                 up = true;
-                right=false;
-                down=false;
-                left=false;
+                right = false;
+                down = false;
+                left = false;
             }
 
             if ((key == KeyEvent.VK_DOWN) && (!up)) {
                 down = true;
-                right=false;
-                up=false;
-                left=false;
+                right = false;
+                up = false;
+                left = false;
             }
         }
     }
